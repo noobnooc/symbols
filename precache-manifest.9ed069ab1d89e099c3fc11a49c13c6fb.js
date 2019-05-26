8309d58dbaa078a5917625474f8fb0afcf4af0e3
Hardo <i@hardo.me>
2019-05-26 23:49:28 +0800

    Updates

---
+++ precache-manifest.9ed069ab1d89e099c3fc11a49c13c6fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5edce5846c4cbe121d40f7305de09d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b5f77ba1df6a33fdef",
+    "url": "/static/css/main.4eed48d0.chunk.css"
+  },
+  {
+    "revision": "07d735dad548342ed405",
+    "url": "/static/js/2.4e3f8710.chunk.js"
+  },
+  {
+    "revision": "57b5f77ba1df6a33fdef",
+    "url": "/static/js/main.5d896bfc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc29f978943cc91634611e54d68aeb7a",
+    "url": "/static/media/logo.fc29f978.svg"
+  }
+]);
